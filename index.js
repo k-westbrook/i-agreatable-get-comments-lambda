92dcea632feb3246c10a3fcb3eebb0fff362b43f
k-westbrook <kcheriewestbrook@gmail.com>
2019-11-26 12:46:49 -0800

    updated file to params

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var { Client } = require('pg')
 exports.handler = async function (event) {
   try {
-
-
     const client = new Client({
       host: process.env.HOST_REFERENCE,
       user: process.env.USER_NAME,
@@ -10,16 +8,16 @@ exports.handler = async function (event) {
       database: process.env.DATABASE_NAME
     });
     await client.connect();
-    let results = await client.query(`SELECT name,comment
-    FROM public."Comments" WHERE restaurant_id = ${event.restaurantId};`);
-
+    let results = await client.query(`SELECT id,name,comment
+    FROM public."Comments" WHERE restaurant_id = ${event.pathParameters.restaurantId};`);
 
     let response =
     {
       headers: {
         'Access-Control-Allow-Origin': '*'
       },
-      body: results.rows
+      body: JSON.stringify(results.rows),
+
     };
     return response;
 

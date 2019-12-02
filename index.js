ff078687da9e377a97d68d1696bf682914c01bd7
k-westbrook <kcheriewestbrook@gmail.com>
2019-12-01 19:29:18 -0800

    client outside of function

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var { Client } = require('pg')
+const client = new Client({
+  host: process.env.HOST_REFERENCE,
+  user: process.env.USER_NAME,
+  password: process.env.PASSWORD_REFERENCE,
+  database: process.env.DATABASE_NAME
+});
+
+client.connect();
+
 exports.handler = async function (event) {
   try {
-    const client = new Client({
-      host: process.env.HOST_REFERENCE,
-      user: process.env.USER_NAME,
-      password: process.env.PASSWORD_REFERENCE,
-      database: process.env.DATABASE_NAME
-    });
-    await client.connect();
+
     let results = await client.query(`SELECT comment_id,name,comment
     FROM public."comment" WHERE restaurant_id = ${event.pathParameters.restaurantId};`);
 
